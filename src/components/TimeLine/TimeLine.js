0703f5af713a0034dad2ce26b30072e68e82fb7e
Matheus Lira <matheustlira@gmail.com>
2021-05-25 17:48:59 -0300

    SideBar implemented in /timeline

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,59 +1,59 @@
 import styled from "styled-components";
-
+import SideBar from "../SideBar/Sidebar";
 export default function TimeLine() {
-  return (
-    <>
-      <Application>
-        <Title>timeline</Title>
-        <Container>
-          <Posts></Posts>
-          <SideBar></SideBar>
-        </Container>
-      </Application>
-    </>
-  );
+    return (
+        <>
+            <Application>
+                <Title>timeline</Title>
+                <Container>
+                    <Posts></Posts>
+                    <SideBar />
+                </Container>
+            </Application>
+        </>
+    );
 }
 
 const Application = styled.div`
-  width: 937px;
-  height: 100%;
-
-  margin: 0 auto;
+    width: 937px;
+    height: 100%;
+    position: relative;
+    margin: 0 auto;
 `;
 
 const Title = styled.div`
-  width: 100%;
-  margin-bottom: 43px;
-  font-size: 43px;
+    width: 100%;
+    margin-bottom: 43px;
+    font-size: 43px;
 
-  font-weight: bold;
-  font-family: "Oswald", sans-serif;
+    font-weight: bold;
+    font-family: "Oswald", sans-serif;
 
-  color: #fff;
+    color: #fff;
 `;
 
 const Container = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const Posts = styled.div`
-  width: 611px;
-  height: 200px;
-  background-color: #333;
+    width: 611px;
+    height: 200px;
+    background-color: #333;
 
-  @media (max-width: 640px) {
-    width: 100%;
-  }
+    @media (max-width: 640px) {
+        width: 100%;
+    }
 `;
 
-const SideBar = styled.div`
-  width: 301px;
-  height: 200px;
-  background-color: blue;
+// const SideBar = styled.div`
+//   width: 301px;
+//   height: 200px;
+//   background-color: blue;
 
-  @media (max-width: 640px) {
-    display: none;
-  }
-`;
+//   @media (max-width: 640px) {
+//     display: none;
+//   }
+// `;

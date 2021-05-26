9ed67e5eba6d3801a8614016d69527741cc7d78a
Matheus Lira <matheustlira@gmail.com>
2021-05-26 10:43:00 -0300

    Starting layout my-posts

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { useState } from 'react'
+import { useState } from "react";
 import styled from "styled-components";
 
 import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
 import SideBar from "./components/SideBar/Sidebar";
 import TimeLine from "./components/TimeLine/TimeLine";
-import Mypost from "./components/MyPost/MyPost";
+import MyPost from "./components/MyPost/MyPost";
 import MyFriendPosts from "./components/MyFriendPosts/MyFriendPosts";
 import HashTag from "./components/HashTag/HashTag";
 import MyLikes from "./components/MyLikes/MyLikes";
 import NavBar from "./components/NavBar";
 import "./styles/reset.css";
-import UserContext from "./context/UserContext"
+import UserContext from "./context/UserContext";
 
 export default function App() {
-  const [accountInformation, setAccountInformation] = useState(null);
+    const [accountInformation, setAccountInformation] = useState(null);
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <UserContext.Provider
-          value={{ accountInformation, setAccountInformation }}
-        >
-          <Route path="/" exact>
-            <Login />
-          </Route>
-          <Route path="/sign-up" exact>
-            <Registration />
-          </Route>
-          <Container>
-            <Route path="/timeline" exact>
-              <NavBar />
-              <TimeLine />
-            </Route>
-            <Route path="/my-posts" exact>
-              <NavBar />
-              <Mypost />
-            </Route>
-            <Route path="/hashtag/:hashtag" exact>
-              <NavBar />
-              <HashTag />
-            </Route>
-            <Route path="/user/:id" exact>
-              <NavBar />
-              <MyFriendPosts />
-            </Route>
-            <Route path="/my-likes" exact>
-              <NavBar />
-              <MyLikes />
-            </Route>
-            <Route path="/siderbar" exact>
-              <SideBar />
-            </Route>
-          </Container>
-        </UserContext.Provider>
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <UserContext.Provider
+                    value={{ accountInformation, setAccountInformation }}
+                >
+                    <Route path="/" exact>
+                        <Login />
+                    </Route>
+                    <Route path="/sign-up" exact>
+                        <Registration />
+                    </Route>
+                    <Container>
+                        <Route path="/timeline" exact>
+                            <NavBar />
+                            <TimeLine />
+                        </Route>
+                        <Route path="/my-posts" exact>
+                            <NavBar />
+                            <MyPost />
+                        </Route>
+                        <Route path="/hashtag/:hashtag" exact>
+                            <NavBar />
+                            <HashTag />
+                        </Route>
+                        <Route path="/user/:id" exact>
+                            <NavBar />
+                            <MyFriendPosts />
+                        </Route>
+                        <Route path="/my-likes" exact>
+                            <NavBar />
+                            <MyLikes />
+                        </Route>
+                        <Route path="/siderbar" exact>
+                            <SideBar />
+                        </Route>
+                    </Container>
+                </UserContext.Provider>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 const Container = styled.div`
-  height: 100vh;
-  background: #333333;
-  padding-top: 125px;
+    height: 100vh;
+    background: #333333;
+    padding-top: 125px;
 `;
--- src/components/MyPost/MyPost.js
+++ src/components/MyPost/MyPost.js
@@ -1,5 +1,51 @@
+import SideBar from "../SideBar/Sidebar";
+import styled from "styled-components";
+
 export default function MyPosts() {
-    return(
-        <div>Olá mundo</div>
-    )
+    return (
+        <>
+            <Application>
+                <Title>my posts</Title>
+                <Container>
+                    <Posts></Posts>
+                    <SideBar />
+                </Container>
+            </Application>
+        </>
+    );
 }
+
+const Application = styled.div`
+    width: 937px;
+    height: 100%;
+    position: relative;
+    margin: 0 auto;
+`;
+
+const Title = styled.div`
+    width: 100%;
+    margin-bottom: 43px;
+    font-size: 43px;
+
+    font-weight: bold;
+    font-family: "Oswald", sans-serif;
+
+    color: #fff;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Posts = styled.div`
+    width: 611px;
+    height: 276px;
+    background-color: #171717;
+    border-radius: 16px;
+
+    @media (max-width: 640px) {
+        width: 100%;
+    }
+`;

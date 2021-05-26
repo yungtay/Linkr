a4c0caba27a88d191342b223ff88cbec45684a5d
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-05-25 23:23:31 -0300

    Inicio da rota Hashtag, request funcionando, porém falta a rota TimeLine para se espelhar

--- src/components/HashTag/HashTag.js
+++ src/components/HashTag/HashTag.js
@@ -1,4 +1,20 @@
+//import styled from "styled-components";
+import axios from 'axios'
+import { useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import UserContext from "../../context/UserContext"
 export default function HashTag() {
+    const { accountInformation } = useContext(UserContext);
+    const { hashtag } = useParams();
+    const config = {headers: { Authorization: `Bearer ${accountInformation.token}` }};
+    console.log(accountInformation)
+
+    useEffect(() => {
+        const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/${hashtag}/posts`, config)
+        request.then((r) => console.log(r.data))
+        request.catch((e) => console.log(e.data))
+    },[])
+
     return(
         <div>Olá mundo</div>
     )

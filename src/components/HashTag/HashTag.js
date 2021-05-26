90512df3f9262cd5e6db06f63099f4dd87cd58d7
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-05-25 23:35:18 -0300

    Resolvendo warnings

--- src/components/HashTag/HashTag.js
+++ src/components/HashTag/HashTag.js
@@ -6,14 +6,13 @@ import UserContext from "../../context/UserContext"
 export default function HashTag() {
     const { accountInformation } = useContext(UserContext);
     const { hashtag } = useParams();
-    const config = {headers: { Authorization: `Bearer ${accountInformation.token}` }};
-    console.log(accountInformation)
 
     useEffect(() => {
+        const config = {headers: { Authorization: `Bearer ${accountInformation.token}` }};
         const request = axios.get(`https://mock-api.bootcamp.respondeai.com.br/api/v2/linkr/hashtags/${hashtag}/posts`, config)
         request.then((r) => console.log(r.data))
         request.catch((e) => console.log(e.data))
-    },[])
+    },[hashtag, accountInformation])
 
     return(
         <div>Olá mundo</div>

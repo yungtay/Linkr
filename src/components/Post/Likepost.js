9d7e8723500057070bf6cd2379c39da997006f76
Fernando Valani Cavalcante <valanife@gmail.com>
2021-05-28 15:02:07 -0300

    tooltip without voce

--- src/components/Post/Likepost.js
+++ src/components/Post/Likepost.js
@@ -24,12 +24,11 @@ export default function Likepost({ posts, likes, setLikes }) {
         [],
         config
       );
-      request.then(() => ReactTooltip.rebuild());
+      request.then();
       request.catch(() => {
         alert("Erro ao curtir");
         setToggle(!toggle);
         setLikes(likes - 1);
-        ReactTooltip.rebuild();
       });
     } else {
       setLikes(likes - 1);
@@ -38,90 +37,12 @@ export default function Likepost({ posts, likes, setLikes }) {
         [],
         config
       );
-      request.then(() => ReactTooltip.rebuild());
+      request.then();
       request.catch(() => {
         alert("Erro ao descurtir");
         setToggle(!toggle);
         setLikes(likes + 1);
-        ReactTooltip.rebuild();
       });
-    }
-  }
-
-  function changePosition(arr, from, to) {
-    arr.splice(to, 0, arr.splice(from, 1)[0]);
-    return arr;
-  }
-
-  let result;
-  returnToolTip();
-  function returnToolTip() {
-    if (toggle) {
-      if (posts.likes.length > 2) {
-        if (posts.likes.username === undefined) {
-          const index = posts.likes
-            .map((item) => item.username)
-            .indexOf(`${accountInformation.user.username}`);
-          const arr = changePosition(
-            posts.likes.map((item) => item.username),
-            index,
-            0
-          );
-          result = `Você, ${arr[1]} e outras ${arr.length - 2} pessoas`;
-        } else {
-          const index = posts.likes
-            .map((item) => item["user.username"])
-            .indexOf(`${accountInformation.user.username}`);
-          const arr = changePosition(
-            posts.likes.map((item) => item["user.username"]),
-            index,
-            0
-          );
-          arr[0] = "Você";
-          result = `${arr[0]}, ${arr[1]} e outras ${arr.length - 2} pessoas`;
-        }
-      } else if (posts.likes.length === 2) {
-        if (posts.likes.username === undefined) {
-          const index = posts.likes
-            .map((item) => item.username)
-            .indexOf(`${accountInformation.user.username}`);
-          const arr = changePosition(
-            posts.likes.map((item) => item.username),
-            index,
-            0
-          );
-          arr[0] = "Você";
-          result = `${arr[0]} e ${arr[1]}`;
-        } else {
-          const index = posts.likes
-            .map((item) => item["user.username"])
-            .indexOf(`${accountInformation.user.username}`);
-          const arr = changePosition(
-            posts.likes.map((item) => item["user.username"]),
-            index,
-            0
-          );
-          arr[0] = "Você";
-          result = `${arr[0]} e ${arr[1]}`;
-        }
-      } else if (posts.likes.length === 1) {
-        result = "Você";
-      }
-    } else {
-      result =
-        posts.likes.length > 2
-          ? `${
-              posts.likes[0]["user.username"] === undefined
-                ? posts.likes[0].username
-                : posts.likes[0]["user.username"]
-            }, ${
-              posts.likes[1]["user.username"] === undefined
-                ? posts.likes[1].username
-                : posts.likes[1]["user.username"]
-            } e outras ${posts.likes.length - 2} pessoas`
-          : posts.likes.map(
-              (item) => " " + (item["user.username"] || item.username)
-            );
     }
   }
 
@@ -142,7 +63,23 @@ export default function Likepost({ posts, likes, setLikes }) {
           width="20px"
         />
       )}
-      <p data-tip={result}>
+      <p
+        data-tip={
+          posts.likes.length > 2
+            ? `${
+                posts.likes[0]["user.username"] === undefined
+                  ? posts.likes[0].username
+                  : posts.likes[0]["user.username"]
+              }, ${
+                posts.likes[1]["user.username"] === undefined
+                  ? posts.likes[1].username
+                  : posts.likes[1]["user.username"]
+              } e outras ${posts.likes.length - 2} pessoas`
+            : posts.likes.map(
+                (item) => " " + (item["user.username"] || item.username)
+              )
+        }
+      >
         {likes === 1 ? `${likes} like` : `${likes} likes`}
       </p>
       <ReactTooltip
